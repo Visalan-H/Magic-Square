6307f1754cb1f0c237bab0dbe799cd389d346453
Visalan <visalanoffl@gmail.com>
2024-06-28 21:12:48 +0530

    final

--- headache.js
+++ headache.js
@@ -1,24 +1,30 @@
-let yy,mm,dd,y1,y2;
+let yy, mm, dd, y1, y2;
+let mntext=document.getElementById("magicnumber");
+let magicnumm=document.getElementById("mn");
+let i2=document.getElementById("inst2");
 
-
-function getdate(){
+function getdate() {
     const dob = document.getElementById("bday").value;
-    const datelist = dob.split('-');
-    yy = datelist[0];
-    mm = datelist[1];
-    dd = datelist[2];
-    // console.log(y);
-    // console.log(m);
-    // console.log(d);
-    // console.log(datelist);
-    y1 = yy.slice(0, 2);
-    y2 = yy.slice(2, 4);
-    // console.log(y1,y2);
-    magic_square();
+    if (dob) {
+        const datelist = dob.split('-');
+        yy = datelist[0];
+        mm = datelist[1];
+        dd = datelist[2];
+        // console.log(y);
+        // console.log(m);
+        // console.log(d);
+        // console.log(datelist);
+        y1 = yy.slice(0, 2);
+        y2 = yy.slice(2, 4);
+        // console.log(y1,y2);
+        magic_square();
+    }
+    else {
+        console.log("ENTER BIRTHDAY");
+    }
 }
 
-function magic_square()
-{
+function magic_square() {
     const a = document.getElementById("item1");
     const b = document.getElementById("item2");
     const c = document.getElementById("item3");
@@ -36,20 +42,23 @@ function magic_square()
     const o = document.getElementById("item15");
     const p = document.getElementById("item16");
 
-    a.innerHTML=dd;
-    b.innerHTML=mm;
-    c.innerHTML=y1;
-    d.innerHTML=y2;
-    e.innerHTML=Number(y2)+1;
-    f.innerHTML=Number(y1)-1;
-    g.innerHTML=Number(mm)-3;
-    h.innerHTML=Number(dd)+3;
-    i.innerHTML=Number(mm)-2;
-    j.innerHTML=Number(dd)+2;
-    k.innerHTML=Number(y2)+2;
-    l.innerHTML=Number(y1)-2;
-    m.innerHTML=Number(y1)+1;
-    n.innerHTML=Number(y2)-1;
-    o.innerHTML=Number(dd)+1;
-    p.innerHTML=Number(mm)-1;
+    a.innerHTML = dd;
+    b.innerHTML = mm;
+    c.innerHTML = y1;
+    d.innerHTML = y2;
+    e.innerHTML = Number(y2) + 1;
+    f.innerHTML = Number(y1) - 1;
+    g.innerHTML = Number(mm) - 3;
+    h.innerHTML = Number(dd) + 3;
+    i.innerHTML = Number(mm) - 2;
+    j.innerHTML = Number(dd) + 2;
+    k.innerHTML = Number(y2) + 2;
+    l.innerHTML = Number(y1) - 2;
+    m.innerHTML = Number(y1) + 1;
+    n.innerHTML = Number(y2) - 1;
+    o.innerHTML = Number(dd) + 1;
+    p.innerHTML = Number(mm) - 1;
+    magicnumm.innerText=Number(dd)+Number(mm)+Number(y1)+Number(y2);
+    mntext.style.visibility="Visible";
+    i2.style.visibility="Visible";
 }
